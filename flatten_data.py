5eadd1730173dfd5155efe7ed2a2ef250b0f7080
Andrei Apostol <andrei.apostol@omnimodular.com>
2023-03-22 00:31:21 +0200

    add script to flatten and dump data

---
+++ flatten_data.py
@@ -0,0 +1,17 @@
+import json5
+import json
+
+# Open with json5 due to some trailing commas
+with open('data/dataset.json', 'r') as f:
+    data = json5.load(f)
+
+flattened_data = []
+
+for item in data:
+    if isinstance(item, dict):
+        flattened_data.append(item)
+    elif isinstance(item, list):
+        flattened_data.extend(item)
+
+with open('data/dataset_flat.json', 'w', encoding='utf-8') as f:
+    json.dump(flattened_data, f, ensure_ascii=False, indent=4)
