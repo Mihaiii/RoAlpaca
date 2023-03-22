8ad9fe76c649491baa5f251e2d4792ead43ce1f9
Mihai <mihai.chirculescu@yahoo.com>
2023-03-22 11:35:44 +0200

    Move scripts to scripts folder and update paths

--- scripts/flatten_data.py
+++ scripts/flatten_data.py
@@ -2,7 +2,7 @@ import json5
 import json
 
 # Open with json5 due to some trailing commas
-with open('data/dataset.json', 'r', encoding="utf8") as f:
+with open('../data/dataset.json', 'r', encoding="utf8") as f:
     data = json5.load(f)
 
 flattened_data = []
@@ -13,5 +13,5 @@ for item in data:
     elif isinstance(item, list):
         flattened_data.extend(item)
 
-with open('data/dataset_flat.json', 'w', encoding='utf-8') as f:
+with open('../data/dataset_flat.json', 'w', encoding='utf-8') as f:
     json.dump(flattened_data, f, ensure_ascii=False, indent=4)
--- scripts/get_all_keys.js
+++ scripts/get_all_keys.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const mySet1 = new Set();
 
-let rawdata = fs.readFileSync('data/dataset_flat.json');
+let rawdata = fs.readFileSync('../data/dataset_flat.json');
 let arrayData = JSON.parse(rawdata);
 
 arrayData.forEach((z) => Object.keys(z).forEach((y) => mySet1.add(y)));
